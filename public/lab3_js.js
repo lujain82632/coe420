3d4e46d627ce12e26a1553095c49eac5778e4991
IsraHasan <g00085609@aus.edu>
2022-02-12 20:52:54 +0400

    chanfesss idk if puahed before

--- public/lab3_js.js
+++ public/lab3_js.js
@@ -1,5 +1,41 @@
-/**
- * 
- */
- 
+
+function increaseSize(){
+	var p = document.getElementsByTagName('div');
+	   for(i=0;i<p.length;i++) {
+	      p[i].style.fontSize = "xx-large";
+	   }
+							
+}
+function resetSize(){
+	var p = document.getElementsByTagName('div');
+	   for(i=0;i<p.length;i++) {
+	      p[i].style.fontSize = "inherit";
+	   }
+}
+function fontType(){
+	var p = document.getElementsByTagName('div');
+	   for(i=0;i<p.length;i++) {
+	      p[i].style.fontFamily="Comic Sans MS, Comic Sans;";
+	   }
+}
+function darkMode(){
+	var element = document.body;
+	element.classList.toggle("dark");
+
+}
+
+function confirmBox(){
+	 var text;
+	 if(submit("Do you want to continue?")){
+		text="You pressed Ok";
+		}
+	else{
+		text="You pressed cancel"
+	}
+	document.getElementByClass("form-group").outerHTML=text;
+}
+			
+  
+		        
+	       
  
