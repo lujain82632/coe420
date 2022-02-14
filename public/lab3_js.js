07822530623cb7006e9bf896bd053822520b3a15
IsraHasan <g00085609@aus.edu>
2022-02-14 22:09:10 +0400

    new stuffffffffffffff

--- public/lab3_js.js
+++ public/lab3_js.js
@@ -1,17 +1,33 @@
-
+/*
 function increaseSize(){
 	var p = document.getElementsByTagName('div');
 	   for(i=0;i<p.length;i++) {
 	      p[i].style.fontSize = "xx-large";
 	   }
-	   var q = document.getElementsById('body-class');
+	   var q = document.getElementsByTagName('body');
 	   for(i=0;i<q.length;i++) {
 	      q[i].style.fontSize = "xx-large";
 	   }
 							
 }
+*/
+function increaseSize(element){
+    var currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+    if (currentSize) {    
+        currentSize = parseFloat(currentSize.replace("px",""));
+        element.style.fontSize = (currentSize * 1.2) + "px";
+        for(var i=0; i < element.children.length; i++){
+            changeFontSize(element.children[i]);
+        }
+    }
+}
+
 function resetSize(){
 	var p = document.getElementsByTagName('div');
+	   for(i=0;i<p.length;i++) {
+	      p[i].style.fontSize = "inherit";
+	   }
+	   var p = document.getElementsByTagName('body');
 	   for(i=0;i<p.length;i++) {
 	      p[i].style.fontSize = "inherit";
 	   }
